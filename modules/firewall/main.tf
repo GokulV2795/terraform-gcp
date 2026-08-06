cb9e7b19463ce7a6d7dc02da92443a7c6abe696b
Gokulv2795 <gokulv388905@gmail.com>
2026-08-06 16:10:54 +0530

    Refactor firewall rules to include SSH, HTTP, and HTTPS configurations

--- modules/firewall/main.tf
+++ modules/firewall/main.tf
@@ -1,9 +1,48 @@
-resource "google_compute_firewall" "internal" {
-
-  name    = "allow-internal"
-
+resource "google_compute_firewall" "ssh" {
+  name    = "allow-ssh"
   network = var.network_name
+  project = var.project_id
 
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = var.source_ranges
+  target_tags   = var.target_tags
+}
+
+resource "google_compute_firewall" "http" {
+  name    = "allow-http"
+  network = var.network_name
+  project = var.project_id
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+  source_ranges = var.source_ranges
+  target_tags   = var.target_tags
+}
+
+resource "google_compute_firewall" "https" {
+  name    = "allow-https"
+  network = var.network_name
+  project = var.project_id
+
+  allow {
+    protocol = "tcp"
+    ports    = ["443"]
+  }
+
+  source_ranges = var.source_ranges
+  target_tags   = var.target_tags
+}
+
+resource "google_compute_firewall" "internal" {
+  name    = "allow-internal"
+  network = var.network_name
   project = var.project_id
 
   allow {
