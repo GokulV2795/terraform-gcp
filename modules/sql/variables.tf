dc59324623312c5635bdc35bdca4240d0187fe19
Gokulv2795 <gokulv388905@gmail.com>
2026-08-07 15:59:06 +0530

    Add SQL module variables for configuration

--- modules/sql/variables.tf
+++ modules/sql/variables.tf
@@ -1,1 +1,36 @@
 # sql module variables
+variable "project_id" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "instance_name" {
+  type = string
+}
+
+variable "database_version" {
+  type = string
+}
+
+variable "tier" {
+  type = string
+}
+
+variable "disk_size" {
+  type = number
+}
+
+variable "network_self_link" {
+  type = string
+}
+
+variable "database_name" {
+  type = string
+}
+
+variable "database_user" {
+  type = string
+}
